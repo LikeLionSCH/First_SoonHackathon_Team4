a7d8ff137797499fb88db8bedf42757074358ec1
Chae hee chan <eqholic1125@gmail.com>
2021-07-03 00:12:32 +0900

    [채희찬][개발]::modles admin 변경

--- hiver/models.py
+++ hiver/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Product(modles.Model):
+class Product(models.Model):
     title = models.CharField(max_length=100)
     price = models.IntegerField()
-    image = models.ImageField(upload_to = "product/", black=True, null=True)
+    image = models.ImageField(upload_to = "product/", blank=True, null=True)
 
     def __str__(self):
         return self.title
